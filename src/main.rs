5440d0070e90e2c68ae5be0064f8844f8f60b665
Sergey <xxsmotur@gmail.com>
2021-01-16 22:58:03 +0500

    Add method for load omages

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use image::io::Reader as ImageReader;
 use std::path::Path;
 use std::ffi::OsStr;
+use std::env;
 
 
-fn load_image(path:&str) -> Result<bool, &str>{
-    let extension = validate_extension(path)?;
-    //let img = ImageReader::open(path)?.decode()?;
-
+fn load_image(path:&str) {
+    let extension = validate_extension(path);
+    ImageReader::open(path)?.decode()
 }
 
 // provides validate of extension
@@ -20,7 +20,17 @@ fn validate_extension(path: &str) -> Result<bool, &str> {
     }
 }
 
+fn diff(first_image: &str, second_image: &str) {
+
+}
+
 fn main() {
-    load_image("playbook.jpg");
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return
+    }
+    let first_image = &args[1];
+    let second_image = &args[2];
+    diff(first_image, second_image)
+    println!("{}", &args[1]);
 }
